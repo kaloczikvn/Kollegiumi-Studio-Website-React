1b3d9fc9051b8457578bea53d97c80de5905b6b1
kaloczikvn <kaloczi.kvn@gmail.com>
2022-03-08 19:15:54 +0100

    style fixes

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,63 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 
-import logo from "../../assets/img/logo.png";
 import {
     VolumeUpIcon,
     UsersIcon,
     PhoneIcon,
     CalendarIcon
 } from "react-line-awesome";
+import { Link } from "react-scroll";
+
+import logo from "../../assets/img/logo.png";
+import vinyl from "../../assets/img/vinyl.png";
+import stylus from "../../assets/img/stylus.png";
 
 import "./Nav.scss";
 
-const Nav: React.FC = () => (
-    <>
-        <div className="nav">
-            <div className="container">
-                <div className="nav-menu">
-                    <a href="#" className="nav-link">
-                        <CalendarIcon />
-                        <span>
-                            Események
-                        </span>
-                    </a>
-                    <a href="#" className="nav-link">
-                        <VolumeUpIcon />
-                        <span>
-                            Rólunk
-                        </span>
-                    </a>
-                    <a href="#" className="nav-link">
-                        <UsersIcon />
-                        <span>
-                            Csapatunk
-                        </span>
-                    </a>
-                    <a href="#" className="nav-link">
-                        <PhoneIcon />
-                        <span>
-                            Kapcsolat
-                        </span>
-                    </a>
-                </div>
-            </div>
+const Nav: React.FC = () => {
+    const [vinylShow, setVinylShow] = useState(false);
 
-            {/*<div className="brand">
-                <img src={logo} alt="Logo" />
+    return (
+        <>
+            <div className="nav">
+                <div className="container">
+                    <div className="nav-menu">
+                        <Link
+                            activeClass="active"
+                            to="events"
+                            className="nav-link"
+                            duration={400}
+                            offset={-150}
+                            spy
+                            smooth
+                        >
+                            <CalendarIcon />
+                            <span>
+                                Események
+                            </span>
+                        </Link>
+                        <Link
+                            activeClass="active"
+                            to="aboutus"
+                            className="nav-link"
+                            duration={400}
+                            offset={-150}
+                            spy
+                            smooth
+                        >
+                            <VolumeUpIcon />
+                            <span>
+                                Rólunk
+                            </span>
+                        </Link>
+                        <Link
+                            activeClass="active"
+                            to="team"
+                            className="nav-link"
+                            duration={400}
+                            offset={-150}
+                            spy
+                            smooth
+                        >
+                            <UsersIcon />
+                            <span>
+                                Csapatunk
+                            </span>
+                        </Link>
+                        <Link
+                            activeClass="active"
+                            to="contact"
+                            className="nav-link"
+                            duration={400}
+                            offset={-150}
+                            spy
+                            smooth
+                        >
+                            <PhoneIcon />
+                            <span>
+                                Kapcsolat
+                            </span>
+                        </Link>
+                    </div>
+                </div>
+
+                {/*<div className="brand">
+                    <img src={logo} alt="Logo" />
+                </div>
+                <a href="#">
+                    asd
+                </a>
+                <a href="#">
+                    asd
+                </a>*/}
             </div>
-            <a href="#">
-                asd
-            </a>
-            <a href="#">
-                asd
-            </a>*/}
-        </div>
-        <div className="container">
-            <a className="nav-brand" href="#">
-                <img src={logo} alt="Kollégiumi Stúdió" />
-            </a>
-        </div>
-    </>
-);
+            <div className="container">
+                <Link
+                    to="events"
+                    className="nav-brand"
+                    duration={400}
+                    smooth
+                >
+                    <img src={vinyl} alt="" className="vinyl" />
+                    <img src={stylus} alt="" className="stylus" />
+                    <img src={logo} alt="Kollégiumi Stúdió" className="logo" />
+                </Link>
+            </div>
+        </>
+    );
+};
 
 export  default Nav;
